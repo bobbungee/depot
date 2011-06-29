b4b3f949be306948df7155cd8f193170f73d99bc
Rishi Shah <bobbungee@gmail.com>
2011-06-28 23:46:23 -0500

    Added dynamic Date

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,12 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(function() {
+  var now = new Date();
+  setTimeout(time, (60-now.getSeconds())*1000);
+});
+
+function time() {
+  var now = new Date();
+  $('#side span#date').text(dateFormat(now, "mmmm d, h:MM TT"));
+  setTimeout(time, 60000);
+}

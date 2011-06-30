e328734446184f2e04589c2d33a2136cd43f88ef
Rishi Shah <bobbungee@gmail.com>
2011-06-30 15:47:19 -0500

    Finding a Cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110622034910) do
+ActiveRecord::Schema.define(:version => 20110630203202) do
+
+  create_table "carts", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", :force => true do |t|
     t.string   "title"

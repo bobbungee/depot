eb0f3c016967d7586fc5aaf3b33f0fa538b40ac2
Rishi Shah <bobbungee@gmail.com>
2011-06-30 19:29:15 -0500

    Added UI improvements

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,20 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(function() {
+  // Date
   var now = new Date();
   setTimeout(time, (60-now.getSeconds())*1000);
+  
+  // CSS Cycle
+  $(".entry").hover(
+    function() {
+      $(this).addClass('hover');
+    },
+    
+    function() {
+      $(this).removeClass('hover');
+    }
+  );
 });
 
 function time() {
